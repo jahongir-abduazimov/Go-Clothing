f5a10dca6643983bd244f16da259400240f3ce7a
jakhong1r-dev <jahongirabduazimov707@gmail.com>
2024-05-24 00:58:40 +0500

    Update

--- src/service/auth/index.ts
+++
@@ -1,9 +0,0 @@
-import { request } from "../config";
-import { Request } from "@/src/interface";
-
-export const auth:Request={
-    signin: (data)=> request.post("/login",data),
-    signup: (data)=> request.post("/signup",data),
-    signout: ()=> request.post("/signout"),
-    reset: (data)=> request.post("/reset", data),
-}
